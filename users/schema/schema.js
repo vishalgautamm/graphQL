ea347157d3cc687d79f5e44a1a7686fc4a34228e
vishalgautamm <vishal.g@live.ca>
2017-04-04 23:43:47 -0400

    Modified schema and server.js

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,27 +1,59 @@
 //KEY - schema contains all of the knowledge required for telling GraphQL exactly what your applications data looks like, including what properties each object has AND exactly how each object object is related to each other.
-
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
-  // to instruct GraphQL about the presence of a user, the idea of a a user
+  // to instruct GraphQL about the presence of a user
   GraphQLObjectType,
   GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
 
-// This object instructs GraphQL about what the User object will look like
+const users = [
+  {id: '23', firstName: 'Bill', age: 20},
+  {id: '47', firstName: 'Samantha', age: 21}
+]
+
+// This tells GraphQL that our app has the concept of a User and each User has an id, firstName and age
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    // fields propery is the most important property here
-    // This tells GraphQL about all the different properties that a User has
-    // keys of this object is the names of the properties that the user has
-    // This tells graphQL that every single user will have an id, firstName and an age. Thats it
-    // value of his object is the type of data each of the fields are. To communicate this to GraphQL we will use the built in types
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
-    // this communicates to GraphQL that every User will user will have an id that is a string
-    // every user will have a firstName that is a string and
-    // every User will have an age and it is an Int
-    // we made use of GraphQLString and GraphQLInt, we need to make sure that we import them from graphql library
   }
 });
+
+// Root Query
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}}, // arguments that are required for a root query
+      resolve(parentValue, args) {
+        return _.find(users, {id: args.id });
+      }
+    }
+  }
+});
+
+/*
+Explanation:
+
+- You can ask me (the RootQuerty) about users in the applicaiton
+- If you give me (args) the id of the user that you are looking for, I will return a user (UserType) back to you
+
+- Resolve function
+  - takes in two arguments - parentValue, args
+  - Purpose: if you are looking for a User with an ID of 23, okay I will do my best to find it
+  - it is where we actually go into our database and find the data that we are actually looking for
+  - parentValue: never really used, ignore it
+*/
+
+// GraphQLSchema -> takes in a Root Query and returns a GraphQLSchema instance
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
+
--- users/server.js
+++ users/server.js
@@ -1,12 +1,14 @@
 // All the logic related to the express part is going to live
 const express = require ('express');
-
 // Library that helps registering graphQL to express
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }))
 
